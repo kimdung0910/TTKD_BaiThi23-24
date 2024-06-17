a8b1f9c3f28ee47af66ebca8efb6bbe86a8bbfd0
kimdung0910 <kimdung09102002@gmail.com>
2024-06-17 13:36:32 +0700

    final commit

---
+++ Models/Person.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace DemoMVC.Models
+{
+    [Table("Person")]
+    public class Person
+    {
+        [Key]
+         public string PersonId { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+  
+
+    }
+}
